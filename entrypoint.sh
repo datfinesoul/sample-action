3ccc0f91fdfb49c34a03f4d4f4dcec65f8ede956
Philip Hadviger <philip.hadviger@phad.me>
2020-12-08 13:28:52 +0900

    save to test linter

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,20 +16,37 @@ source <( \
   jq -r 'to_entries | .[] | "export " + .key + "=\"" + .value + "\""' \
   )
 
-if [[ "${GITHUB_REPOSITORY}" =~ [^/]+\/gds.clusterconfig.(.*) ]]; then
+if [[ -n "${INPUT_CLUSTER:-}" ]]; then
+  # override if provided via the action
+  CLUSTER="${INPUT_CLUSTER}"
+elif [[ "${GITHUB_REPOSITORY}" =~ [^/]+\/gds.clusterconfig.(.*) ]]; then
+  # otherwise it has to match the gds clusterconfig repo name syntax
   CLUSTER="${BASH_REMATCH[1]}"
 fi
+
+
 for FILE in $(find . -type f -name orders -maxdepth 2); do
+  # service is based on directory name
   SERVICE="$(basename $(dirname $FILE))"
-  IFS=' \t' read -r TYPE REPOSITORY <<< \
+
+  # parse out the deploy commands we support
+  IFS=$' \t' read -r TYPE REPOSITORY <<< \
     "$(grep -e "^\(auto\|docker\)deploy\s" $FILE | tail -n1)"
+
+  if [[ "${TYPE}" == "dockerdeploy" ]]; then
+    # eg. github/glg/epi-screamer/gds-migration:latest
+    true
+  fi
+
   if [[ "${TYPE}" == "autodeploy" ]]; then
+    # eg. git@github.com:glg/log.git#master
     true
     # there is a generated ecr repo here
   fi
+
+  # TODO: get ecr repo tag (latest, sha)
+  echo "$CLUSTER,$SERVICE,$TYPE,$REPOSITORY"
 done
-# TODO: get ecr repo tag (latest, sha)
-echo "$CLUSTER,$SERVICE,$TYPE,$REPOSITORY"
 
 #curl \
 #  --silent \
