246f82dd76fc4810cbdec5dbe863008bd67ac928
Philip Hadviger <philip.hadviger@phad.me>
2020-12-08 10:15:31 +0900

    save work

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,26 @@
-#!/bin/sh -l
+#!/bin/bash -l
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+#echo "Hello $1"
+#time=$(date)
+#echo "::set-output name=time::$time"
 
-echo "${INPUT_SUMOLOGIC_CONFIG}" | jq -rM '.' | base64 > /tmp/boo
-cat /tmp/boo
+set -o nounset;
+set -o errexit;
+set -o pipefail;
+
+IFS=$'\n\t'
+
+# load the sumolgic config into environment variables
+source <( \
+  echo "${INPUT_SUMOLOGIC_CONFIG}" | \
+  jq -r 'to_entries | .[] | "export " + .key + "=\"" + .value + "\""' \
+  )
+
+env | grep SUMO
+
+curl \
+  --silent \
+  --show-error \
+  --user "${SUMOLOGIC_ACCESS_ID}:${SUMOLOGIC_ACCESS_KEY}" \
+  -XGET \
+  "${SUMOLOGIC_API_ENDPOINT}/v2/content/folders/global"
