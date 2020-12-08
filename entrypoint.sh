2c4dfdf99a0ebe6da2391aff5a5d9c34899baf76
Philip Hadviger <philip.hadviger@phad.me>
2020-12-08 14:48:46 +0900

    alpha

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,6 +26,7 @@ fi
 
 
 for FILE in $(find . -type f -name orders -maxdepth 2); do
+  unset SERVICE ECR_REPO GIT_REPO GIT_BRANCH TYPE REPOSITORY
   # service is based on directory name
   SERVICE="$(basename $(dirname $FILE))"
 
@@ -35,22 +36,50 @@ for FILE in $(find . -type f -name orders -maxdepth 2); do
 
   if [[ "${TYPE}" == "dockerdeploy" ]]; then
     # eg. github/glg/epi-screamer/gds-migration:latest
-    true
+    if [[ "${REPOSITORY}" =~ ([^/]+)\/([^/]+)\/([^/]+)\/([^:]+)(:(.*))? ]]; then
+      #                      ↑        ↑        ↑        ↑      ↑ ↑
+      #                      |        |        |        |      5 6 docker tag
+      #                      |        2 org    3 repo   4 branch
+      #                      1 source control provider (eg. github)
+      ECR_REPO="${REPOSITORY}"
+      ECR_REPO="${BASH_REMATCH[1]}/${BASH_REMATCH[2]}/${BASH_REMATCH[3]}"
+      # ERE does not support non-capturing groups, that's why branch is in 6
+      ECR_TAG="${BASH_REMATCH[6]:-latest}"
+      GIT_REPO="${BASH_REMATCH[2]}/${BASH_REMATCH[3]}"
+      GIT_BRANCH="${BASH_REMATCH[4]}"
+    fi
   fi
 
   if [[ "${TYPE}" == "autodeploy" ]]; then
+    # NOTE: currently does not support autodeploy without #branch specification
     # eg. git@github.com:glg/log.git#master
-    true
-    # there is a generated ecr repo here
+    if [[ "${REPOSITORY}" =~ git@github.com:([^#]+).git#(.*) ]]; then
+      #                                     ↑           ↑
+      #                                     1 org/repo  2 branch
+      ECR_REPO="${CLUSTER}.glgresearch.com/${SERVICE}"
+      ECR_TAG="latest"
+      GIT_REPO="${BASH_REMATCH[1]}"
+      GIT_BRANCH="${BASH_REMATCH[2]}"
+    fi
   fi
 
-  # TODO: get ecr repo tag (latest, sha)
-  echo "$CLUSTER,$SERVICE,$TYPE,$REPOSITORY"
-done
+  if [[ -z "${GIT_REPO:-}" ]]; then
+    echo "error: ${CLUSTER}/${FILE}: unable to extract GIT_REPO"
+    continue
+  fi
 
-#curl \
-#  --silent \
-#  --show-error \
-#  --user "${SUMOLOGIC_ACCESS_ID}:${SUMOLOGIC_ACCESS_KEY}" \
-#  -XGET \
-#  "${SUMOLOGIC_API_ENDPOINT}/v2/content/folders/global"
+  # NOTE: hardcoded for now, but likely going into a secret as well
+  TABLE_ID="0000000000F6A3B4"
+  curl \
+    -XPUT \
+    --header 'Content-Type: application/json' \
+    --silent \
+    --show-error \
+    --user "${SUMOLOGIC_ACCESS_ID}:${SUMOLOGIC_ACCESS_KEY}" \
+    --data @/tmp/payload \
+    --output /tmp/output \
+    --write-out "status_code:%{http_code} [$CLUSTER,$SERVICE],$TYPE,[$ECR_REPO,$ECR_TAG],[$GIT_REPO,$GIT_BRANCH]\n" \
+    "${SUMOLOGIC_API_ENDPOINT}/v1/lookupTables/${TABLE_ID}/row" \
+    || true
+
+done
